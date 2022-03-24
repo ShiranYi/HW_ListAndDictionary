69f5526811a84fd373b648bbe863bf0d33d831e6
Shiran Yitzhak <shiran.yitzhak@gmail.com>
2022-03-24 21:48:58 +0200

    Add project files.

---
+++ HW_ListAndDictionary/MyLibrary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_ListAndDictionary
+{
+    internal class MyLibrary
+    {
+        private Dictionary<string, Book> books;
+        public int Count { get { return books.Count; } }
+
+        public MyLibrary()
+        {
+            books = new Dictionary<string, Book>();
+        }
+        public bool AddBook(Book book)
+        {
+            if (book == null)
+                return books.TryAdd(book.Title, book);
+            return false;
+
+        }
+        public bool RemoveBook(string book)
+        {
+            if (book == null)
+                return false;
+            return books.Remove(book);
+
+        }
+        public bool HaveThisBook(string book)
+        {
+            return books.ContainsKey(book);
+        }
+        public Book GetBook(string book)
+        {
+            return books[book];
+        }
+   
+        public void Clear()
+        {
+            books = new Dictionary<string, Book>();
+
+        }
+        public List<string> GetAuthors()
+        {
+            List<string> authors = new List<string>();
+            foreach (Book book in books.Values)
+            {
+                authors.Add(book.Author);
+            }
+
+            return authors;
+
+        }
+        public List<Book> GetBooksSortedByAuthorName()
+        {
+            return books.Values.OrderBy(book => book.Author).ToList();
+        }
+    
+        public override string ToString()
+        {
+            string bookList = "";
+            foreach (KeyValuePair<string, Book> book in books)
+            {
+                bookList += book.Key + ": " + book.Value + "\n";
+            }
+            return bookList;
+        }
+
+
+
+
+    }
+}
---
+++ HW_ListAndDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_ListAndDictionary
+{
+    internal class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Book1", "abcd", "Dana", "Adventure");
+            Book book2 = new Book("Book2", "abcd", "Yossi", "Thriller");
+            Book book3 = new Book("Book3", "abcd", "Moshe", "Science Fiction");
+            Book book4 = new Book("Book4", "abcd", "Shira", "Novel");
+
+            Dictionary<string, Book> books = new Dictionary<string, Book>();
+            books.Add("Book1", book1);
+            books.Add("Book2", book2);
+            books.Add("Book3", book3);
+            books.Add("Book4", book4);
+
+    
+            
+            
+
+
+
+
+        }
+    }
+}
